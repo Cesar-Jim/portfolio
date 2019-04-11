767c691355c6df756b9499c997414238c9ece767
Cesar Jimenez <cijgr@hotmail.com>
2019-04-11 08:51:33 -0500

    Setup and main routing complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
 import "./App.css";
 
+// Importing Route and Link. Required for proper routing
+import { Route, Link } from "react-router-dom";
+
+// Importing components
+import Contact from "./components/Contact";
+import Landing from "./components/Landing";
+import Resume from "./components/Resume";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>Hello World</p>
+        <header>
+          <nav>
+            <Link to="/">Home</Link> | <Link to="/resume"> Resume</Link> |{" "}
+            <Link to="/contact">Contact</Link>
+          </nav>
+          <h1>Cesar Jimenez</h1>
+        </header>
+        <main>
+          <Route exact path="/" component={Landing} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/resume" component={Resume} />
+        </main>
       </div>
     );
   }
