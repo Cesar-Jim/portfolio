d5905b573777f603905a5770db9b626a29a6ec11
Cesar Jimenez <cijgr@hotmail.com>
2019-04-24 11:56:01 -0500

    Ready to deply after restructure

---
+++ build/precache-manifest.2522139e944690e40b83673bf5115246.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "638381ed7aeaabe46dd52ba9981f87a6",
+    "url": "/static/media/two-hero.638381ed.jpg"
+  },
+  {
+    "revision": "6629c59a79f6019134d5a63bd753ad72",
+    "url": "/static/media/skills.6629c59a.png"
+  },
+  {
+    "revision": "5ef3e1bcbb5d7221ddf79c88cbc9b510",
+    "url": "/static/media/one-hero.5ef3e1bc.jpg"
+  },
+  {
+    "revision": "3f6d89bef17af2dca5cdbb8325b8f877",
+    "url": "/static/media/cesar-picture-no-shadow.3f6d89be.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e63ec72b56e8e215079b",
+    "url": "/static/js/main.8734012a.chunk.js"
+  },
+  {
+    "revision": "d9ac9a5e758e055977f0",
+    "url": "/static/js/2.e5e7361b.chunk.js"
+  },
+  {
+    "revision": "e63ec72b56e8e215079b",
+    "url": "/static/css/main.6dc3a7e7.chunk.css"
+  },
+  {
+    "revision": "d9ac9a5e758e055977f0",
+    "url": "/static/css/2.12bba210.chunk.css"
+  },
+  {
+    "revision": "a25910b691dcf7e2ab103cf80ed64950",
+    "url": "/index.html"
+  }
+];
