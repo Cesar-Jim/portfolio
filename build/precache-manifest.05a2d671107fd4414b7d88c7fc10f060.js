278deb0197ed192620a46af8fa3bcfb19d5feb19
Cesar Jimenez <cijgr@hotmail.com>
2019-05-06 21:47:52 -0500

    Ready to deploy after CSS fixes. Margins and paddings

---
+++ build/precache-manifest.05a2d671107fd4414b7d88c7fc10f060.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "61fb957e3256940a69b366ec67b11c7d",
+    "url": "/static/media/three-hero.61fb957e.jpg"
+  },
+  {
+    "revision": "712a110f84bcfde8cc98",
+    "url": "/static/css/main.b722397c.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a7345b033a595e2b72a3ece6e1d3aa25",
+    "url": "/static/media/five-hero.a7345b03.jpg"
+  },
+  {
+    "revision": "2a3a96a471623a9da987",
+    "url": "/static/js/2.ece21ea7.chunk.js"
+  },
+  {
+    "revision": "3f6d89bef17af2dca5cdbb8325b8f877",
+    "url": "/static/media/cesar-picture-no-shadow.3f6d89be.png"
+  },
+  {
+    "revision": "5ef3e1bcbb5d7221ddf79c88cbc9b510",
+    "url": "/static/media/one-hero.5ef3e1bc.jpg"
+  },
+  {
+    "revision": "638381ed7aeaabe46dd52ba9981f87a6",
+    "url": "/static/media/two-hero.638381ed.jpg"
+  },
+  {
+    "revision": "712a110f84bcfde8cc98",
+    "url": "/static/js/main.a882fc37.chunk.js"
+  },
+  {
+    "revision": "3ae69cb6b079cd9a0b20124214be55f5",
+    "url": "/static/media/tribeapp.3ae69cb6.jpg"
+  },
+  {
+    "revision": "bb3b104061c1bf2e19e60609202d4b09",
+    "url": "/static/media/uva.bb3b1040.jpg"
+  },
+  {
+    "revision": "92e69f4ba51ac8044e880408c08dfdfe",
+    "url": "/static/media/jams.92e69f4b.jpg"
+  },
+  {
+    "revision": "fdcc2188108a314d5d688a15d40c3ea1",
+    "url": "/static/media/blocipedia.fdcc2188.jpg"
+  },
+  {
+    "revision": "9a015f14893be734620606282b4a2538",
+    "url": "/static/media/four-hero.9a015f14.jpg"
+  },
+  {
+    "revision": "2a3a96a471623a9da987",
+    "url": "/static/css/2.12bba210.chunk.css"
+  },
+  {
+    "revision": "c535f35d113a580328a2ef811042eb02",
+    "url": "/index.html"
+  }
+];
